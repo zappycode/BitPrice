ac4d0bdaa2177ae21508dd46e2390041e7588aac
Nick Walter <nick@zappycode.com>
2016-10-07 16:36:10 -0600

    Fin!

--- BitPrice WatchKit Extension/ComplicationController.swift
+++ BitPrice WatchKit Extension/ComplicationController.swift
@@ -14,43 +14,84 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Configuration
     
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.forward, .backward])
-    }
-    
-    func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
-    }
-    
-    func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
-    }
-    
-    func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
-        handler(.showOnLockScreen)
+        handler([])
     }
     
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries prior to the given date
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries after to the given date
-        handler(nil)
+        
+        let url = URL(string: "https://api.coindesk.com/v1/bpi/currentprice.json")!
+        
+        URLSession.shared.dataTask(with: url) { (data:Data?, response:URLResponse?, error:Error?) in
+            if error == nil {
+                print("It worked")
+                
+                if data != nil {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+                        
+                        guard let bpi = json["bpi"] as? [String:Any], let USD = bpi["USD"] as? [String:Any], let price = USD["rate_float"] as? NSNumber else {
+                            return
+                        }
+                        
+                        let intPrice = Int(price)
+                        
+                        if complication.family == .modularSmall {
+                            let template = CLKComplicationTemplateModularSmallStackText()
+                            template.line1TextProvider = CLKSimpleTextProvider(text: "BIT")
+                            template.line2TextProvider = CLKSimpleTextProvider(text: "\(intPrice)")
+                            
+                            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+                            handler(entry)
+                        }
+                        if complication.family == .modularLarge {
+                            let template = CLKComplicationTemplateModularLargeStandardBody()
+                            template.headerTextProvider = CLKSimpleTextProvider(text: "BitPrice")
+                            
+                            let formatter = NumberFormatter()
+                            formatter.numberStyle = .currency
+                            formatter.locale = Locale(identifier: "en_US")
+                            
+                            template.body1TextProvider = CLKSimpleTextProvider(text: formatter.string(from: price)!)
+                            
+                            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+                            handler(entry)
+                        }
+                        
+                    } catch {}
+                    
+                    
+                }
+                
+            } else {
+                print("It's broke!")
+            }
+            }.resume()
+        
+        
+        
     }
     
     // MARK: - Placeholder Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        
+        if complication.family == .modularSmall {
+            let template = CLKComplicationTemplateModularSmallStackText()
+            template.line1TextProvider = CLKSimpleTextProvider(text: "BIT")
+            template.line2TextProvider = CLKSimpleTextProvider(text: "$$$")
+            handler(template)
+        }
+        if complication.family == .modularLarge {
+            let template = CLKComplicationTemplateModularLargeStandardBody()
+            template.headerTextProvider = CLKSimpleTextProvider(text: "BitPrice")
+            template.body1TextProvider = CLKSimpleTextProvider(text: "$1,456.78")
+            
+            handler(template)
+        }
     }
     
 }
--- BitPrice WatchKit Extension/InterfaceController.swift
+++ BitPrice WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,75 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
+    
+    @IBOutlet var updatingLabel: WKInterfaceLabel!
+    @IBOutlet var priceLabel: WKInterfaceLabel!
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        if let price = UserDefaults.standard.value(forKey: "price") as? NSNumber {
+            // We have a previous price
+            updatingLabel.setText("Updating...")
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .currency
+            formatter.locale = Locale(identifier: "en_US")
+            
+            self.priceLabel.setText(formatter.string(from: price))
+        } else {
+            priceLabel.setText("Getting Price...")
+            updatingLabel.setText("")
+        }
+        
+        getPrice()
+        
+        
+        
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    func getPrice() {
+        
+        let url = URL(string: "https://api.coindesk.com/v1/bpi/currentprice.json")!
+        
+        URLSession.shared.dataTask(with: url) { (data:Data?, response:URLResponse?, error:Error?) in
+            if error == nil {
+                print("It worked")
+                
+                if data != nil {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+                        
+                        guard let bpi = json["bpi"] as? [String:Any], let USD = bpi["USD"] as? [String:Any], let price = USD["rate_float"] as? NSNumber else {
+                            return
+                        }
+                        
+                        let formatter = NumberFormatter()
+                        formatter.numberStyle = .currency
+                        formatter.locale = Locale(identifier: "en_US")
+                        
+                        self.priceLabel.setText(formatter.string(from: price))
+                        self.updatingLabel.setText("Updated")
+                        
+                        UserDefaults.standard.set(price, forKey: "price")
+                        UserDefaults.standard.synchronize()
+                        
+                        if CLKComplicationServer.sharedInstance().activeComplications != nil {
+                            
+                            for comp in CLKComplicationServer.sharedInstance().activeComplications! {
+                                CLKComplicationServer.sharedInstance().reloadTimeline(for: comp)
+                            }
+                        }
+                        
+                    } catch {}
+                    
+                    
+                }
+                
+            } else {
+                print("It's broke!")
+            }
+            }.resume()
     }
-
+    
 }
